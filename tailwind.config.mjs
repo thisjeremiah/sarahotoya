9abbc96aa2375b479074193b2ba5a3147e291ed4
Jeremiah Montoya <jeremiahmontoya@hey.com>
2023-12-06 23:09:44 -0800

    add some initial content

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,22 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		colors: {
+			yellow: {
+				100: '#FFFDF7',
+				500: "#FFF4D1",
+				700: "#F8D561"
+			},
+			black: {
+				DEFAULT: '#000000',
+				400: '#272727',
+			},
+		},
+		extend: {
+			borderRadius: {
+				'50': '3.125rem' // 50px
+			}
+		},
 	},
 	plugins: [],
 }
