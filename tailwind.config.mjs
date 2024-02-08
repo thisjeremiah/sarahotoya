3ad0620a300d36e70b4f2d8f1ffe658fc19d1d09
Jeremiah Montoya <jeremiahmontoya@hey.com>
2024-02-07 20:19:40 -0800

    update styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -21,6 +21,21 @@ export default {
 		extend: {
 			borderRadius: {
 				'50': '3.125rem' // 50px
+			},
+			animation: {
+				'bounce-x': 'bounce-x 1s infinite',
+			},
+			keyframes: {
+				'bounce-x': {
+					'0%, 100%': {
+						transform: 'translateX(0)',
+						animationTimingFunction: 'cubic-bezier(0, 0, 0.2, 1)',
+					},
+					'50%': {
+						transform: 'translateX(-10%)',
+						animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)',
+					}
+				}
 			}
 		},
 	},
